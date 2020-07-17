de7167ee8bf22de19275ed0d5b30eabb944060bc
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-07-16 18:18:16 -0700

    added two more tests, one to change the input values on the form and another to check if the submit button fires, all tests passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -13,3 +13,27 @@ test("Testing the form inputs", () => {
   const lastNameInput = getByLabelText(/Last Name/i);
   const emailInput = getByText(/email/i);
 })
+
+test("Test if we can change input values", () => {
+  const {getByLabelText} = render(<ContactForm />)
+
+  const firstNameInput = getByLabelText(/First Name/)
+  fireEvent.change(firstNameInput, {target: {value: 'Danielle'}});
+  expect(firstNameInput.value).toBe('Danielle');
+
+  const lastNameInput = getByLabelText(/Last Name/i)
+  fireEvent.change(lastNameInput, {target: {value: 'Mathey'}});
+  expect(lastNameInput.value).toBe('Mathey');
+
+  const emailInput = getByLabelText(/Email/i)
+  fireEvent.change(emailInput, {target: {value: "email@test.com"}});
+  expect(emailInput.value).toBe('email@test.com');
+
+})
+
+test("See if the submit button works", () => {
+  const {getByTestId} = render(<ContactForm />)
+  
+  const submitButton = getByTestId('test');
+  fireEvent.click(submitButton);
+})
