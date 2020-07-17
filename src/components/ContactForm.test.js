6036b39546c235af3edd0984e4b2679c74dafd8f
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-07-16 17:57:07 -0700

    added test to check the form inputs

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,3 +5,11 @@ import {render, fireEvent} from '@testing-library/react';
 test("See if the app is working", () => {
   render(<ContactForm />)
 })
+
+test("Testing the form inputs", () => {
+  const {getByLabelText, getByText} = render(<ContactForm />)
+
+  const firstNameInput = getByLabelText(/First Name/i);
+  const lastNameInput = getByLabelText(/Last Name/i);
+  const emailInput = getByText(/email/i);
+})
